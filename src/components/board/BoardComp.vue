6ec5969d222663106e48cd7b52c8dd859666b214
Carlos Daniel Angel Castaño <ingeniero.desarrollo4@comersantander.com>
2024-02-08 10:22:45 -0500

    feat: prepare board and step for new interfaces

--- src/components/board/BoardComp.vue
+++ src/components/board/BoardComp.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
+import type { Step } from '@/interfaces/Step'
+
 import StepComp from '@/components/board/StepComp.vue'
+
+const steps: Step[] = [
+  {
+    title: 'Pending',
+    tasks: [
+      { task: 'Task 1', description: 'description 1' },
+      { task: 'Task 2', description: 'description 2' },
+      { task: 'Task 3', description: 'description 3' }
+    ]
+  },
+  {
+    title: 'Processed',
+    tasks: [
+      { task: 'Task 1', description: 'description 1' },
+      { task: 'Task 2', description: 'description 2' },
+      { task: 'Task 3', description: 'description 3' }
+    ]
+  },
+  {
+    title: 'Finish',
+    tasks: [
+      { task: 'Task 1', description: 'description 1' },
+      { task: 'Task 2', description: 'description 2' },
+      { task: 'Task 3', description: 'description 3' }
+    ]
+  },
+  {
+    title: 'Stopped',
+    tasks: [
+      { task: 'Task 1', description: 'description 1' },
+      { task: 'Task 2', description: 'description 2' },
+      { task: 'Task 3', description: 'description 3' }
+    ]
+  }
+]
 </script>
 
 <template>
   <h3>Board</h3>
-  <StepComp />
-  <StepComp />
-  <StepComp />
-  <StepComp />
-  <StepComp />
+  <StepComp v-for="(item, index) in steps" :key="index" :step="item" />
 </template>
--- src/components/board/StepComp.vue
+++ src/components/board/StepComp.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Step } from '../../interfaces/Step'
+
+const props = defineProps<{
+  step: Step
+}>()
+</script>
 
 <template>
-  <h4>Step</h4>
+  <h4>{{ props.step.title }}</h4>
+  <ul>
+    <li v-for="(item, index) in props.step.tasks" :key="index">
+      <b>item.title</b>
+      <p>item.description</p>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+h4 {
+  color: red;
+}
+
+h5 {
+  color: blue;
+}
+</style>
